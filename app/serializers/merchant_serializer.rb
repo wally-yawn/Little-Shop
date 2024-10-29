503ba64539c15a16176fbea58888e001597e19a7
Wally Wallace <jrvw001@gmail.com>
2024-10-29 16:51:05 -0600

    Add test for no merchants

--- app/serializers/merchant_serializer.rb
+++ app/serializers/merchant_serializer.rb
@@ -1,6 +1,15 @@
 class MerchantSerializer
 
   def self.format_merchants(merchants)
-    puts 'Hi Wally'
+    merchant_data = merchants.map do |merchant|
+      {
+        id: merchant.id,
+        type: "merchant",
+        attributes: {
+          name: merchant.name
+        }
+      }
+    end
+    {data: merchant_data}
   end
 end
