6093ac6fe4f1554015b46db746ab4b7b070080cc
Wally Wallace <jrvw001@gmail.com>
2024-10-30 15:45:56 -0600

    Create tests for get merchant by id

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -33,5 +33,22 @@ RSpec.describe "Merchants API" do
       merchants = JSON.parse(response.body)
       expect(merchants["data"].count).to eq(0)
     end
+
+    it 'can fetch a single merchant by id' do
+      get "/api/v1/merchants/#{@merchant1.id}"
+      expect(response).to be_successful
+      merchant = JSON.parse(response.body)
+
+      expect(merchant["data"]).to have_key("id")
+      expect(merchant["data"]["id"])..to be_a(Integer)
+      expect(merchant["data"])..to have_key("type")
+      expect(merchant["data"]["type"]).to eq("merchant")
+      expect(merchant["data"]["attributes"]).to have_key("name")
+      expect(merchant["data"]["attributes"]["name"]).to eq("Wally")
+    end
+
+    it "returns an error when the merchant_id does not exist" do
+      expect(true).to eq(false)
+    end
   end
 end
