3dd8c3fdde60013cd00d2d9532642a0846b455e7
James-Cochran <jameslikesfruit@gmail.com>
2024-10-30 16:43:57 -0400

    added update action to controller with private strong params method

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -4,5 +4,15 @@ class Api::V1::MerchantsController < ApplicationController
     render json: MerchantSerializer.format_merchants(Merchant.all)
   end
 
-  
+  def update
+    merchant = Merchant.find(params[:id])
+    merchant.update(merchant_params)
+    render json:MerchantSerializer.format_merchants([merchant])
+  end
+
+  private
+
+  def merchant_params
+    params.permit(:name)
+  end
 end
