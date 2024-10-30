0e1f776603319c613b6b514e95576d6f5747e046
James-Cochran <jameslikesfruit@gmail.com>
2024-10-30 16:22:33 -0400

    added route for merchants to index action

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   get "/api/v1/merchants", to: "api/v1/merchants#index"
+  patch "/api/v1/merchants/:id", to: "api/v1/merchants#update"
 end
