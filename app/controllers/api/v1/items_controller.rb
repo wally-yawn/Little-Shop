37f63887bce1d50598d533c2924311c42aa4909f
Jonathan Atkins <jonathanjatkins89@gmail.com>
2024-10-30 21:25:10 -0700

    feat: get_all_items/get_one_item

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,12 +1,16 @@
 class Api::V1::ItemsController < ApplicationController
   def index
-    items = Item.all
-    render json: items
-  end  
+    if params[:sorted] == 'price'
+      items = Item.order(:unit_price)
+    else
+      items = Item.all
+    end
+    render json: ItemSerializer.format_items(items)
+  end
 
   def show
     item = Item.find(params[:id])
-    render json: item, status: :ok
+    render json: ItemSerializer.format_single_item(item)
   rescue ActiveRecord::RecordNotFound
     render json: { error: 'Item not found' }, status: :not_found
   end  
---
+++ app/serializers/item_serializer.rb
@@ -0,0 +1,23 @@
+class ItemSerializer
+
+    def self.format_items(items)
+        { data: items.map { |item| formatted_item(item) }}
+    end
+
+    def self.format_single_item(item)
+        { data: formatted_item(item) }
+    end
+
+    def self.formatted_item(item)
+        {
+            id: "#{item.id}",
+            type: 'item',
+            attributes: {
+                name: item.name,
+                description: item.description,
+                unit_price: item.unit_price,
+                merchant_id: item.merchant_id
+            }
+        }
+    end
+  end
