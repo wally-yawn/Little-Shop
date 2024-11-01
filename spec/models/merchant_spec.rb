022aadfd8911e392ad65c24e9ba7f4a331a1f1d4
James-Cochran <jameslikesfruit@gmail.com>
2024-11-01 19:03:36 -0400

    added validation test to the merchant model.  Added create happy and sad tests in requests.

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -5,6 +5,10 @@ RSpec.describe Merchant, type: :model do
     it { should have_many(:items)}
   end
 
+  describe "validations" do
+    it { should validate_presence_of(:name) }
+  end
+
   describe "sort" do
     it "can sort merchants based on age" do
       @merchant1 = Merchant.create(name: 'Wally')
