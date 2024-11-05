32ead3f9ef7ca33183fc1ce31f420287a82f76ee
Wally Wallace <jrvw001@gmail.com>
2024-11-04 21:28:27 -0700

    Add missing invoice association to merchant model

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,17 +1,15 @@
 class Merchant < ApplicationRecord
   validates :name, presence: true
-
-  has_many :items
-
+  has_many :invoices
   has_many :items, dependent: :destroy
-
   has_many :customers
-
-
 
   def self.sort(params)
     if params[:sorted] == "age"
       Merchant.all.order(id: :desc)
+    elsif params[:status] == "returned"
+      merchants_with_returns = Merchant.joins(:invoices).where("invoice.status = 'returned'")
+      binding.pry
     else
       Merchant.all
     end
