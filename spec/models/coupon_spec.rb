49ccdf54e8a81ce8b583e6284fe68c40fe2c3e88
Wally Wallace <jrvw001@gmail.com>
2024-11-09 17:01:15 -0700

    Add model test for countInvoices

--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -5,4 +5,19 @@ RSpec.describe Coupon, type: :model do
     it { should have_many(:invoices)}
     it { should belong_to(:merchant)}
   end
+
+  before :each do
+    @merchant1 = Merchant.create!(name: 'Wally Wallace')
+    @merchant2 = Merchant.create!(name: 'Dahlia Wallace')
+    @coupon1 = Coupon.create!(name: "Coupon 1", merchant_id: @merchant1.id, status: "active", code: "COUP1", off: 5, percent_or_dollar: "percent")
+    @customer1 = Customer.create!(first_name: "Wally", last_name: "Wallace")
+    @invoice1 = Invoice.create!(customer: @customer1, merchant: @merchant1, status: "returned", coupon: @coupon1)
+    @invoice2 = Invoice.create!(customer: @customer1, merchant: @merchant1, status: "shipped")
+    end
+
+  describe 'countInvoices' do
+    it 'returns the count of invoices with this coupon' do
+      expect(@coupon1.countInvoices).to eq(1)
+    end
+  end
 end
