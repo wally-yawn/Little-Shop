3c4decd87d73d55541beee114bc3bbedf931e8df
Wally Wallace <jrvw001@gmail.com>
2024-11-09 21:43:05 -0700

    Add request tests for activate

--- spec/requests/api/v1/coupon_request_spec.rb
+++ spec/requests/api/v1/coupon_request_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe "Coupons API", type: :request do
     @merchant1 = Merchant.create!(name: 'Wally Wallace')
     @coupon1 = Coupon.create!(name: "Coupon 1", merchant_id: @merchant1.id, status: "active", code: "COUP1", off: 5, percent_or_dollar: "percent")
     @customer1 = Customer.create!(first_name: "Wally", last_name: "Wallace")
+    @coupon2 = Coupon.create!(name: "Coupon 2", merchant_id: @merchant1.id, status: "inactive", code: "COUP2", off: 5, percent_or_dollar: "percent")
   end
 
   describe 'show' do
@@ -132,7 +133,6 @@ RSpec.describe "Coupons API", type: :request do
       expect(coupon[:attributes][:status]).to eq("inactive")
       @coupon1.reload
       expect(@coupon1.status).to eq("inactive")
-
     end
 
     it 'cannot deactivate an active coupon with pending invoices' do
@@ -174,6 +174,63 @@ RSpec.describe "Coupons API", type: :request do
       @coupon1.destroy
 
       patch "/api/v1/coupons/#{@coupon1.id}/deactivate"
+      expect(response).to_not be_successful
+      expect(response.status).to eq(404)
+
+      data = JSON.parse(response.body, symbolize_names: true)
+
+      expect(data[:message]).to eq("your request could not be completed") 
+      expect(data[:errors]).to be_a(Array)
+  
+      error = data[:errors].first
+      expect(error[:status]).to eq("404")
+      expect(error[:title]).to eq("Couldn't find Coupon with 'id'=#{missing_id}") 
+    end
+  end
+
+  describe 'activate' do
+    it 'can activate an inactive coupon' do
+      patch "/api/v1/coupons/#{@coupon2.id}/activate"
+
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+      
+      coupon = JSON.parse(response.body, symbolize_names: true)[:data]
+
+      expect(coupon[:id]).to eq(@coupon2.id.to_s)
+      expect(coupon[:attributes][:status]).to eq("active")
+      @coupon2.reload
+      expect(@coupon2.status).to eq("active")
+
+    end
+
+    it 'cannot activate an inactive coupon if there are already 5 active coupons' do
+      coupon3 = Coupon.create!(name: "Coupon 2", merchant_id: @merchant1.id, status: "active", code: "COUP2", off: 5, percent_or_dollar: "percent")
+      coupon4 = Coupon.create!(name: "Coupon 3", merchant_id: @merchant1.id, status: "active", code: "COUP3", off: 5, percent_or_dollar: "percent")
+      coupon5 = Coupon.create!(name: "Coupon 4", merchant_id: @merchant1.id, status: "active", code: "COUP4", off: 5, percent_or_dollar: "percent")
+      coupon6 = Coupon.create!(name: "Coupon 5", merchant_id: @merchant1.id, status: "active", code: "COUP5", off: 5, percent_or_dollar: "percent")
+      
+      patch "/api/v1/coupons/#{@coupon2.id}/activate"
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(422)
+      
+
+      data = JSON.parse(response.body, symbolize_names: true)
+
+      expect(data[:message]).to eq("your request could not be completed") 
+      expect(data[:errors]).to be_a(Array)
+  
+      error = data[:errors].first
+      expect(error[:status]).to eq("422")
+      expect(error[:title]).to eq("Merchant #{@merchant1.id} already has 5 active coupons") 
+    end
+
+    it 'returns an error if the coupon does not exist' do
+      missing_id = @coupon2.id
+      @coupon2.destroy
+
+      patch "/api/v1/coupons/#{@coupon2.id}/deactivate"
       expect(response).to_not be_successful
       expect(response.status).to eq(404)
 
